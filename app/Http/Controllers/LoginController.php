33a5f272b3c709e435fdd3db93a78dd457c59b2f
marioangelozzi <mario.angelozzi@gmail.com>
2020-03-25 22:41:32 -0300

    sessão / login 3

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,29 +12,33 @@ class LoginController extends Controller
 
     public function checkLogin(Request $request) {
 
-        $usuario = Usuario::where('email','=',$request->email)->get();
+         $usuario = Usuario::where('email','=',$request->email)->get();
+         session_start();
 
          if ( count($usuario)>0) {
 
             $senha = $usuario[0]->senha;
 
              if (password_verify($request->password, $senha)) {
-                if (isset($_SESSION["usuario"])) {
-                    session_unset();
-                    session_destroy();
-                }
-                ini_set("session.cookie_lifetime","60"); 
-                session_start();
+                //ini_set("session.cookie_lifetime","60"); 
+
                 $_SESSION["usuario"] = $usuario[0]->nome;
                 return redirect ('/');
 
             } 
         } else {
+
             if (isset($_SESSION["usuario"])) {
+
+                //ini_set("session.cookie_lifetime","0");
+
+                $_SESSION["usuario"] = null;
                 session_unset();
                 session_destroy();
+
             }
-            return redirect ('/');
+            return redirect()->back() ->with('alert', 'Usuário ou Senha inválidos!');
+            //return redirect ('/');
         }
 
     }
